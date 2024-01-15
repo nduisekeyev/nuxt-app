c6b3a3ea768edf2086747806bd5e74c3cf4b392d
Nur Duisekeyev <nur.duisekeyev@unineedgroup.com>
2024-01-15 22:37:42 +0600

    feat: update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,60 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/devtools", "@nuxtjs/i18n"],
+  typescript: {
+    // typeCheck: true,
+    strict: true,
+  },
+  modules: ["@unocss/nuxt", "@nuxt/devtools", "@nuxtjs/i18n"],
+  components: {
+    dirs: [
+      {
+        path: "~/components",
+        pathPrefix: false,
+        priority: 2,
+      },
+    ],
+    global: true,
+  },
+
+  nitro: {
+    compressPublicAssets: true,
+  },
   devtools: { enabled: true },
   i18n: {
     locales: [
-      { code: "en", iso: "en-US", file: "./i18n/en.json" },
+      { code: "", iso: "en-US", file: "./i18n/en.json" },
       { code: "fr", iso: "fr-FR", file: "./i18n/fr.json" },
       { code: "kz", iso: "kz-KZ", file: "./i18n/kz.json" },
     ],
-    defaultLocale: "en",
-    strategy: "prefix",
-    detectBrowserLanguage: {
-      useCookie: true,
-      cookieKey: "i18n_redirected",
-    },
+    strategy: "prefix_except_default",
+    defaultLocale: "en-GB",
+    detectBrowserLanguage: false,
   },
+  css: [
+    "@unocss/reset/tailwind-compat.css", // needed to reset styles see https://unocss.dev/guide/style-reset (@unocss/reset)
+    "~/assets/css/main.css",
+  ],
   router: {
     options: {
       linkActiveClass: "link-active",
       linkExactActiveClass: "link-exact-active text-primary",
+    },
+  },
+  routeRules: {
+    "/": {
+      isr: 60 * 60 * 24,
+    },
+    "/blog": {
+      ssr: false,
+    },
+    "/about": {
+      ssr: false,
+    },
+    "/insights": {
+      ssr: false,
+    },
+    "/**": {
+      isr: 60 * 60 * 24,
     },
   },
 });
